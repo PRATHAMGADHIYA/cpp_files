e3a8ddeb8dd6068da4c3d8d1bd6fb807d42a1e56
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-02-09 19:37:32 +0530

    practice2

---
+++ second.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout << "enter value of a: ";
+    cin >> a;
+
+    cout << "enter value of b: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Sum is:" << c;
+    cout << endl
+           << "Thanks...." << endl;
+
+     return 0;      
+}
